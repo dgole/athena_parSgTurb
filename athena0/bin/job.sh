fd8b34aa5667d6fc1061c7df658f7bf312cf531c
dgole <dgole100@gmail.com>
2019-01-11 15:32:39 -0700

    final testing

--- athena0/bin/job.sh
+++ athena0/bin/job.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-#SBATCH -J testParticles
-#SBATCH --time=1:00:00
+#SBATCH -J fp10
+#SBATCH --time=24:00:00
 #SBATCH -N 1
 #SBATCH --ntasks 64
 #SBATCH -o out_%j.txt
 #SBATCH -e error_%j.txt
-#SBATCH -p development
+#SBATCH -p normal
 
 # The following commands will be executed when this script is run.
-module load intel impi fftw3 
-ibrun -np 64 ./athena -i athinput.par_strat3d_turb 
+module load intel impi fftw3
+ibrun -np 64 ./athena -i athinput.par_strat3d_turb
 
 
 #
